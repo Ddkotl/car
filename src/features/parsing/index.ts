cd535761b1c35e049377b2818981e187bbb5442e
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-08-14 12:50:24 +0000

    fix

--- src/features/parsing/index.ts
+++ src/features/parsing/index.ts
@@ -1,4 +1,4 @@
-import { Browser, chromium } from "playwright";
+import { Browser, BrowserContext, chromium, Page } from "playwright";
 import { addHTTPheaders } from "./functions/addHttpHeader";
 import { getAllCarsModels } from "./modules/get_all_cars_models";
 import { parseNewsFromManyPages } from "./modules/get_news_with_tag_from_many_pages";
@@ -18,9 +18,17 @@ export async function StartParse() {
 export async function ExeParse() {
   console.log("start parsing");
   let browser: Browser | undefined;
+  let page: Page | undefined;
+  let pageToImages: Page | undefined;
+  let context: BrowserContext | undefined;
+  let contextToImages: BrowserContext | undefined;
   try {
     browser = await chromium.launch({ headless: true });
-    const [page, pageToImages] = await addHTTPheaders(browser, false);
+    const data = await addHTTPheaders(browser, false);
+    page = data.page;
+    pageToImages = data.pageToImages;
+    context = data.context;
+    contextToImages = data.contextToImages;
 
     await parseNewsFromManyPages(page, pageToImages, 1);
     await getAllCarsModels(page, pageToImages);
@@ -28,6 +36,22 @@ export async function ExeParse() {
   } catch (error) {
     console.error("Parsing Error", error);
   } finally {
+    if (page) {
+      page.close();
+      console.log("page closed");
+    }
+    if (pageToImages) {
+      pageToImages.close();
+      console.log("pageToImages closed");
+    }
+    if (context) {
+      context.close();
+      console.log("context closed");
+    }
+    if (contextToImages) {
+      contextToImages.close();
+      console.log("contextToImages closed");
+    }
     if (browser) {
       browser.close();
       console.log("browser closed");
