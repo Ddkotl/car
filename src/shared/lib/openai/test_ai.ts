dfa0f5901b2a1aacc753f7c2d8b8fe5f350b8440
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-08-15 07:38:51 +0000

    fix

--- src/shared/lib/openai/test_ai.ts
+++ src/shared/lib/openai/test_ai.ts
@@ -1,9 +1,15 @@
-import { client } from "./ai_client";
+import { client, TEXT_AI_MODEL } from "./ai_client";
 
 async function test() {
   const res = await client.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [{ role: "user", content: "Hello" }],
+    model: TEXT_AI_MODEL,
+    messages: [
+      {
+        role: "system",
+        content: "Отвечай на русcком языке  строго в  формате строки ,  без добавления комментариев.",
+      },
+      { role: "user", content: "Hello" },
+    ],
   });
   console.log(res.choices[0].message.content);
 }
