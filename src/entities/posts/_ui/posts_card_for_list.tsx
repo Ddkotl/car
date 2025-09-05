a318bd1044d4cd27d29517f0ffae085af3721ae5
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-09-05 17:47:16 +0000

    f

--- src/entities/posts/_ui/posts_card_for_list.tsx
+++ src/entities/posts/_ui/posts_card_for_list.tsx
@@ -41,6 +41,7 @@ export function PostsCardForList({
         {/* Content section */}
         <div className="flex flex-col justify-between p-2 sm:pt-1 sm:p-4 ">
           <div>
+            <div>{SinglePost.type}</div>
             <Link href={`/${SinglePost.type === "NEWS" ? "news" : "reviews"}/${SinglePost.slug}`} className="group">
               <Title
                 size="lg"
