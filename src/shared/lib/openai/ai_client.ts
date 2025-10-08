3a3c42ef0b70ba494a9c96760c91b3d4006beb4b
Dmitry KOTL <134155338+Ddkotl@users.noreply.github.com>
2025-10-08 14:16:32 +0300

    Update ai_client.ts

--- src/shared/lib/openai/ai_client.ts
+++ src/shared/lib/openai/ai_client.ts
@@ -6,5 +6,5 @@ export const client = new OpenAI({
   baseURL: `http://localhost:${privateConfig.G4F_PORT}/v1`,
 });
 
-export const TEXT_AI_MODELS = ["deepseek-v3","deepseek-v3.1","llama","gpt-4"];
+export const TEXT_AI_MODELS = ["gpt-4"];
 export const IMAGE_AI_MODEL = "flux";
